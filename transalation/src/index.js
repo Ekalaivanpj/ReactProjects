7fb730c5531fef94c5e2f722d7012e34ee0c1d30
Logesh P <pjlogeshnila@gmail.com>
2021-09-29 23:14:41 +0530

    jsx implemented

--- transalation/src/index.js
+++ transalation/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const eka = () => {
+    return "Click me!";
+}
+
 const App = () => {
-    return <div>Hi pandiyan</div>;
+    return (
+        <div>
+            <label className="label">EnterName: </label>
+            <input style={{borderColor: 'red'}}/>
+            <button style={{backgroundColor: 'green', color: 'white', marginLeft: '5px'}}> {eka()} </button>
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
