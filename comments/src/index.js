acc42e51bbd197d7e154e9996675760f6a1526e2
Logesh P <pjlogeshnila@gmail.com>
2021-10-08 00:17:44 +0530

    Approval card created

--- comments/src/index.js
+++ comments/src/index.js
@@ -2,13 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetails from './CommentDetails';
 import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return(
         <div className='ui comments'>
-            <CommentDetails author="sam" time="10 min ago" comment="hi my name is sam" img={faker.image.avatar()}/>
-            <CommentDetails author="eka" time="20 min ago" comment="hi sam i am eka" img={faker.image.avatar()}/>
-            <CommentDetails author="pandian"time="30 min ago" comment="hi folks how r u?" img={faker.image.avatar()}/>
+            <ApprovalCard>
+            <CommentDetails 
+                author="sam" 
+                time="10 min ago" 
+                comment="hi my name is sam" 
+                img={faker.image.avatar()}
+            />
+            </ApprovalCard>
+            <ApprovalCard>
+            <CommentDetails 
+                author="eka" 
+                time="20 min ago" 
+                comment="hi sam i am eka" 
+                img={faker.image.avatar()}
+            />
+            </ApprovalCard>
+            <ApprovalCard>
+            <CommentDetails 
+                author="pandian" 
+                time="30 min ago" 
+                comment="hi folks how r u?" 
+                img={faker.image.avatar()}
+            />
+            </ApprovalCard>
         </div>
     );
 }
